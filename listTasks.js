271177c3a6e026c98d80280401fc828c8272bdff
9dbartlett <31480988+9dbartlett@users.noreply.github.com>
2018-04-10 09:21:52 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -1,18 +1,31 @@
 //Task 1
 function largest(list){
-
+  let big# = list[0];
+  for (let a = 0; a <= list.length; a++){
+    if(big# < list[a]){
+      big# = list[a];
+    }
+  }
+  return big#;
 }
-
 //Task 2
 function reverse(list){
-
+  let esrever = [];
+  for( let b = 0; b <= list.length; b++){
+    //list[b].push to esrever
+  return esrever;
+  }
 }
 
 //Task 3
 function cotains(list,e){
-
+  for(let c = 0; c <= list.length; c++){
+    if(list[c] == e){
+      if true(return e){}
+      else(return "this number is not found.")
+    }
+  }
 }
-
 //Task 4
 function oddElements(list){
 
@@ -46,7 +59,7 @@ function weave(listA, listB){
 //Task 10
 //Attempt last
 function mergeInOrder(listA, listB){
-    
+
 }
 
 //Task 11
